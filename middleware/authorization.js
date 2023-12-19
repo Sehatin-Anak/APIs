8041a9e8f69a94f293ae89e19a12c79283a18c2f
sevend07 <benidr071203@gmail.com>
2023-12-19 20:18:07 +0700

    FINAL COMMIT

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,17 +1,20 @@
-
-// ****GANTI PAKE API KEY VALIDATION**** //
-
 require('dotenv').config()
 
 const authorization = async (req, res, next) => {
   const authKey = [process.env.API_KEY]
-  const apiKey = req.query.apiKey
+  const apiKey = req.headers.authorization
+
   try {
-    if (!apiKey || authKey.includes(apiKey)) {
+    if (!apiKey) {
+      throw new Error('Missing required header: Authorization')
+    }
+    const key = apiKey.split(' ')[1]
+
+    if (!key || !authKey.includes(key)) {
       throw new Error('Unauthorized user')
     }
   } catch (error) {
-    return res.status(401).json({
+    return res.status(400).json({
       errorName: error.name,
       errorMessage: error.message
     })
