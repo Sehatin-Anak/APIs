55a89bded6f895e5536ba3f6202cd2601be6b58e
sevend07 <benidr071203@gmail.com>
2023-12-20 00:29:19 +0700

    FINAL COMMIT

--- router/auth.js
+++ router/auth.js
@@ -2,9 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const auth = require("../controller/authController");
-const authorization = require("../middleware/authorization");
-
-// Route for authentication
+const authorization = require('../middleware/authorization')
 
 router.get("/auth/google", authorization, auth);
 
--- router/childBio.js
+++ router/childBio.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const authorization = require("../middleware/authorization");
 const {
